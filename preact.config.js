1e5159e5645f0e7991bb51d5d7ad5b8badfaf590
Nurbek Zhussip <nzhussip@alfabank.kz>
2022-08-10 17:48:23 +0600

    refactor: updpate build filename

--- preact.config.js
+++ preact.config.js
@@ -1,11 +1,28 @@
 const path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 export default (config) => {
     config.entry['bundle'] = path.resolve(__dirname, "./src/index.js"),
     config.output = {
         path: path.resolve(__dirname, 'build'),
-        filename: '[name].js',
+        filename: 'bnpl-plugin.[name].js',
         library: 'MyApp',
         libraryTarget: 'umd',
     }
+
+    config.plugins.push(
+        new MiniCssExtractPlugin({
+            filename: "bnpl-plugin.css",
+            ignoreOrder: false,
+        })
+    );
+    //
+    // config.module = {
+    //     rules: [
+    //         {
+    //             test: /\.css$/i,
+    //             use: [MiniCssExtractPlugin.loader, "css-loader"],
+    //         }
+    //     ],
+    // }
 }
